bf72346bdb00602ae7bc4e8cb7322c2008eb8e62
akDeveloper <andreas.kollaros@gmail.com>
2014-04-30 23:36:46 +0300

    fix autoload

--- test/test_init.php
+++ test/test_init.php
@@ -1,5 +1,7 @@
 <?php
 
-require_once 'larium_form/autoload.php';
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once 'FormClass.php';
